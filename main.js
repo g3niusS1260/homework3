ade2cfae0a60ec4e628ea3ae8d4fa32badca6643
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-15 09:04:53 +0300

    feat: задача "Подсчёт зарплат"

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const salaries = {
+    ivan: 1200,
+    maria: 1500,
+    petr: 1100,
+    anna: 1600
+};
+
+let totalSum = 0;
+
+for(let person in salaries){
+    totalSum += salaries[person];
+}
+
+console.log(totalSum);
