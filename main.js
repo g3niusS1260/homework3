e6d0e28e230880f8964b55b8d3259bd2f84b6144
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-15 08:59:33 +0300

    feat: задача "Автомобиль с методом"

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const car = {
+    brand : 'Bentley',
+    model : 'Flying Spur',
+    year : '2025',
+    isEngineOn : false,
+    startEngine : function(){
+        this.isEngineOn = true;
+        console.log(`Двигатель : ${this.brand} ${this.model} запущен.`);
+    },
+    stopEngine : function(){
+        this.isEngineOn = false;
+        console.log(`Двигатель : ${this.brand} ${this.model} заглушен.`);
+    },
+};
