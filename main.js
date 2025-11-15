3d12d6df9a46ab354c4c73da37be4bdd7febc5f5
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-15 08:46:15 +0300

    feat: первая задача "Профиль пользователя"

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const userProfile = {
+    username : 'michael.jackson',
+    email : 'michaeljackson@gmail.ru',
+    age : 34,
+    isLoggedIn : true
+}
+
+console.log(userProfile.email);
+
+userProfile.age = 26;
+
+userProfile['user location'] = 'New York';
+
+console.log(userProfile['user location']);
+
