8d3899ef55aa537d676e4191a5f0b08215e3d2b7
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-15 12:37:47 +0300

    feat: задача "Список задач"

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+
+let tasks = ["Выучить основы JS", "Помыть посуду", "Купить продукты"];
+
+function addTask(list, taskName){
+    if(list.includes(taskName)){
+        console.log('Такая задача уже есть');
+    }else{
+        list.push(taskName);
+    }
+}
+
+function completeTask(list, taskName){
+    if(list.indexOf(taskName) !== -1){
+        list.splice(list.indexOf(taskName), 1);
+    }else{
+        console.log('Такой задачи не существует');
+    }
+}
+
+addTask(tasks, 'Помыть посуду');
+addTask(tasks, 'Пропылесосить квартиру');
+addTask(tasks, 'Выгулить собаку');
+
+completeTask(tasks, 'Выгулить собаку');
+
+console.log(tasks);
