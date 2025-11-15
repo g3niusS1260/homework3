53d8e1e8cefd4408f06845f9e52f9684e39b6de0
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-15 13:02:36 +0300

    feat: задача "Форматирование имён"

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const names = ["иван", "АННА", "петр", "ОЛЬГА"];
+
+function formatNames(namesArray){
+    const newArray = [];
+    for (let name of namesArray){
+        let firstSymbol = name[0].toUpperCase();
+        let taleName = (name.slice(1)).toLowerCase();
+        newArray.push(firstSymbol.concat(taleName));
+    }
+
+    return newArray;
+}
+
+console.log(formatNames(names));
