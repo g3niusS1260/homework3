46bf821d66a1b2e27a35a1ce1a68c5bd3f487e93
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-17 13:34:40 +0300

    feat: задача "Отбор студентов"

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+const students = [
+  { name: "Иван", score: 120 },
+  { name: "Мария", score: 95 },
+  { name: "Алексей", score: 150 },
+  { name: "Светлана", score: 80 },
+  { name: "Дмитрий", score: 101 }
+];
+
+function getPassingStudents(studentsList, passingScore){
+    const filterArray = studentsList.filter((element) => element.score >= passingScore);
+
+    return filterArray;
+}
+
+console.log(getPassingStudents(students, 100));
